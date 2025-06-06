8047ecb094b61f267027db98d4b94dec729b5e3b
Lowder <me@lowderplay.dev>
2025-06-06 23:57:02 +0500

    fix: interests tags

--- src/lib/components/widgets/InterestsWidget.svelte
+++ src/lib/components/widgets/InterestsWidget.svelte
@@ -1,50 +1,45 @@
 <script lang="ts">
   import { ChevronUp } from '@lucide/svelte';
-  import { fade } from 'svelte/transition';
+  import { cn } from '$lib/utils';
   let { interests }: { interests: { id: string; tag: string }[] } = $props();
   let isOpen = $state(false);
 </script>
 
-<div class="w-full h-30 p-[10px] transition-all" class:h-fit={isOpen}>
+<div class="w-full p-[10px] transition-all relative" class:h-fit={isOpen}>
   <div
-    class="flex flex-row w-fit gap-[8px] flex-wrap h-30 transition-all overflow-hidden"
+    class={cn(
+      'flex flex-row w-fit gap-[8px] flex-wrap transition-all overflow-hidden',
+      !isOpen && 'max-h-25',
+    )}
     class:overflow-visible={isOpen}
-    class:h-fit={isOpen}
-    class:test={!isOpen}
+    class:gradient={!isOpen}
   >
     {#each interests as interest}
       <div
         class="flex gap-[8px] h-fit bg-accent/25 rounded-[28px] px-[12px] py-[8px]"
       >
-        <!--<img
-          class="size-4 my-auto"
-          src="https://cdnjs.cloudflare.com/ajax/libs/emoji-datasource-apple/15.1.2/img/apple/64/{[
-            ...interest[0],
-          ]
-            .map((cp) => cp.codePointAt(0).toString(16))
-            .join('-')}.png" 
-        />-->
         <p class="text-accent">{interest.tag}</p>
       </div>
     {/each}
     {#if isOpen}
       <button
-        class="flex gap-[8px] bg-accent/25 rounded-[28px] h-fit px-[12px] py-[8px]"
+        onclick={() => (isOpen = false)}
+        class="flex gap-[8px] bg-accent/25 rounded-[28px] h-fit px-[12px] py-[8px] text-accent"
       >
-        <p class="text-accent"><ChevronUp /></p>
-        <p class="text-accent" onclick={() => (isOpen = false)}>Свернуть</p>
+        <ChevronUp />
+        Свернуть
       </button>
     {/if}
   </div>
   <button
-    class="flex justify-self-center text-accent relative -top-11"
+    class="justify-center justify-self-center text-accent z-10 absolute bottom-4 left-0 right-0"
     onclick={() => (isOpen = true)}
     class:hidden={isOpen}>Развернуть</button
   >
 </div>
 
 <style>
-  .test {
+  .gradient {
     mask-image: linear-gradient(to top, transparent 1%, white 100%);
   }
 </style>
