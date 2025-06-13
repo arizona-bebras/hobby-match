e0001f9868081b8e7a8608a2149cf444f3df9741
XrystikLK <23spiisiinii@gmail.com>
2025-06-14 03:27:44 +0500

    fix(questionnaire): removed telegram button

--- src/lib/components/profile/Questionnaire.svelte
+++ src/lib/components/profile/Questionnaire.svelte
@@ -20,134 +20,15 @@
   import RenderWidget from '$lib/components/profile/RenderWidget.svelte';
   import type { PageData } from '$lib/questionnaireTypes/questionnaireTypes';
 
-  let changeMode = $state(false);
-  let showWidgetMenu = $state(false);
-  let addedWidget: WidgetType | undefined = $state(undefined);
-  let editingWidget: string | undefined = $state(undefined);
-
   let { data }: { data: PageData } = $props();
-  console.log(data);
-  let widgets = $state(data.widgets);
-  console.log('Data', data);
-  console.log('Widgets:', widgets);
-  useTelegramButton(() => {
-    changeMode = !changeMode;
-  });
-
-  $effect(() => {
-    window.Telegram.WebApp.MainButton.setText(
-      changeMode ? 'Сохранить' : 'Моя анкета',
-    );
-  });
-
-  $effect(() => {
-    if (!addedWidget && !showWidgetMenu)
-      window.Telegram.WebApp.MainButton.show();
-    else window.Telegram.WebApp.MainButton.hide();
-    return () => {
-      window.Telegram.WebApp.MainButton.hide();
-    };
-  });
-
-  $effect(() => {
-    widgets = data.widgets;
-  });
-
-  function onEditClose() {
-    addedWidget = undefined;
-    editingWidget = undefined;
-  }
 </script>
 
-<Header {data} {changeMode} />
+<Header {data} />
 <div class="font-[Inter] px-4 w-full max-w-full relative">
-  <UserInfo {data} {changeMode} />
+  <UserInfo {data} />
 
-  {#if changeMode}
-    <button
-      class="my-2 w-full h-12 bg-[#34C759] rounded-xl flex justify-center items-center gap-3 font-medium"
-      onclick={() => (showWidgetMenu = true)}
-    >
-      <Plus class="size-5" />
-      Добавить виджет
-    </button>
-  {/if}
-
-  <WidgetsListMenu
-    open={showWidgetMenu}
-    onClick={(name) => {
-      showWidgetMenu = false;
-      addedWidget = name;
-    }}
-  />
-
-  <EditVideo
-    open={addedWidget === 'video'}
-    onClose={onEditClose}
-    widgetId={addedWidget === 'video' ? editingWidget : undefined}
-  />
-  <EditProgress
-    open={addedWidget === 'progress_bar'}
-    onClose={onEditClose}
-    widgetId={addedWidget === 'progress_bar' ? editingWidget : undefined}
-  />
-  <EditText
-    open={addedWidget === 'text'}
-    onClose={onEditClose}
-    widgetId={addedWidget === 'text' ? editingWidget : undefined}
-  />
-  <EditSurvey
-    open={addedWidget === 'survey'}
-    onClose={onEditClose}
-    widgetId={addedWidget === 'survey' ? editingWidget : undefined}
-  />
-  <EditSocial
-    open={addedWidget === 'social_media'}
-    onClose={onEditClose}
-    widgetId={addedWidget === 'social_media' ? editingWidget : undefined}
-  />
-  <EditTgPost
-    open={addedWidget === 'post'}
-    onClose={onEditClose}
-    widgetId={addedWidget === 'post' ? editingWidget : undefined}
-  />
-  <EditImage
-    open={addedWidget === 'photo'}
-    onClose={onEditClose}
-    widgetId={addedWidget === 'photo' ? editingWidget : undefined}
-  />
-  <EditToDo
-    open={addedWidget === 'todo'}
-    onClose={onEditClose}
-    widgetId={addedWidget === 'todo' ? editingWidget : undefined}
-  />
-  <EditAudio
-    open={addedWidget === 'audio'}
-    onClose={onEditClose}
-    widgetId={addedWidget === 'audio' ? editingWidget : undefined}
-  />
-  <EditSteamGame
-    open={addedWidget === 'steam_game'}
-    onClose={onEditClose}
-    widgetId={addedWidget === 'steam_game' ? editingWidget : undefined}
-  />
-  {#each widgets as widget, i (widget.id)}
+  {#each data.widgets as widget (widget.id)}
     <div class="relative mb-2">
-      {#if changeMode}
-        <Edit
-          {widget}
-          onEdit={() => {
-            addedWidget = widget.data.type;
-            editingWidget = widget.id;
-          }}
-          onMove={(delta) => {
-            if ((delta < 0 && i <= 0) || (delta > 0 && i >= widgets.length - 1))
-              return;
-
-            [widgets[i + delta], widgets[i]] = [widgets[i], widgets[i + delta]];
-          }}
-        />
-      {/if}
       <RenderWidget {widget} />
     </div>
   {/each}
