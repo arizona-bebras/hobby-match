b27ff93a7a7feb75bfc473c818c61ac85c17db39
XrystikLK <23spiisiinii@gmail.com>
2025-06-18 17:48:52 +0500

    feat(registration): messages about errors

--- src/lib/components/registration/InformationFormShema.ts
+++ src/lib/components/registration/InformationFormShema.ts
@@ -1,12 +1,21 @@
 import { z } from 'zod';
 
 export const informationSchema = z.object({
-  miniapp_name: z.string().min(2).max(50),
+  miniapp_name: z
+    .string()
+    .min(2, 'Длинна должна быть больше 2-ух символов')
+    .max(50, 'Длина не должна быть больше 50-ти символов'),
   // @ts-expect-error sex
   gender: z.enum(['male', 'female']),
   birth_date: z.string().datetime(),
-  location: z.string().min(2).max(50),
-  user_info: z.string().min(2).max(250),
+  location: z
+    .string()
+    .min(1, 'Поле не должно быть пустым')
+    .max(50, 'Длина не должна быть больше 50-ти символов'),
+  user_info: z
+    .string()
+    .min(10, 'Длинна должна быть больше 10-и символов')
+    .max(250, 'Длина не должна быть больше 250-ти символов'),
 });
 
 export type FormSchema = typeof informationSchema;
