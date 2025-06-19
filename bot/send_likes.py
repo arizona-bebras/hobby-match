fa098a9976979299348626fe069837f728999888
shyinakira <maxi.solts@gmail.com>
2025-06-19 23:25:09 +0500

    fix: db_address env var 2

--- bot/send_likes.py
+++ bot/send_likes.py
@@ -6,7 +6,7 @@ from telegram.ext import ContextTypes
 from pocketbase import PocketBase
 import os
 import asyncio
-pb = PocketBase("DB_ADDRESS")
+pb = PocketBase(os.getenv("DB_ADDRESS"))
 DB_ADMIN_LOGIN = os.getenv("DB_ADMIN_LOGIN")
 DB_ADMIN_PASSWORD = os.getenv("DB_ADMIN_PASSWORD")
 bot = Bot(token=os.getenv('BOT_TOKEN'))
