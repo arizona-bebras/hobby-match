8eee5f74a297e17446a407e247d5ab6d2e197ca9
Lowder <me@lowderplay.dev>
2025-07-02 00:03:17 +0500

    fix: simplify feed query

--- backend/pb_hooks/worker.pb.js
+++ backend/pb_hooks/worker.pb.js
@@ -53,94 +53,42 @@ function upsertUser(e) {
 
 routerAdd("GET", "/worker/feed", (e) => {
   const views = arrayOf(new DynamicModel({
-    "pageOwner": "",
-    "view_count": 0,
+    "user_id": "",
+    "last_viewed": "",
   }))
-  // TODO: it might get stuck if there are a lot of missing records in Vectorize
   $app.db().newQuery(`
-      WITH viewer_counts AS (
-          SELECT
-              pageOwner,
-              COUNT(*) AS view_count
-          FROM
-              views
-          WHERE
-              viewer = {:current_user_id}
-          GROUP BY
-              pageOwner
-      ),
-           all_counts AS (
-               SELECT
-                   u.id AS pageOwner,
-                   COALESCE(vc.view_count, 0) AS view_count
-               FROM
-                   users u
-                       LEFT JOIN
-                   viewer_counts vc ON u.id = vc.pageOwner
-               WHERE
-                   u.id != {:current_user_id} 
-                 AND u.miniapp_name <> '' 
-                 AND u.user_photo <> ''
-                 AND u.interests <> '[]'
-                 AND NOT u.hide
-           ),
-           min_count AS (
-               SELECT
-                   MIN(view_count) AS min_view_count
-               FROM
-                   all_counts
-           ),
-           min_count_pages AS (
-               SELECT
-                   pageOwner,
-                   view_count
-               FROM
-                   all_counts
-               WHERE
-                   view_count = (SELECT min_view_count FROM min_count)
-           ),
-           count_min_pages AS (
-               SELECT
-                   COUNT(*) AS cnt
-               FROM
-                   min_count_pages
-           )
-      SELECT
-          pageOwner,
-          view_count
-      FROM
-          min_count_pages
-      WHERE
-              (SELECT cnt FROM count_min_pages) >= 3
-
-      UNION ALL
-
-      SELECT
-          pageOwner,
-          view_count
-      FROM
-          all_counts
-      WHERE
-              (SELECT cnt FROM count_min_pages) < 3
-      ORDER BY
-          view_count ASC,
-          pageOwner ASC
-      LIMIT
-          CASE
-              WHEN (SELECT cnt FROM count_min_pages) >= 3 THEN
-                  CASE
-                      WHEN (SELECT cnt FROM count_min_pages) > 100 THEN 100
-                      ELSE (SELECT cnt FROM count_min_pages)
-                      END
-              ELSE 3
-              END;
+      WITH user_view_dates AS (
+            SELECT
+                u.id AS user_id,
+                MAX(v.created) AS last_viewed
+            FROM users u
+               LEFT JOIN views v
+                   ON v.pageOwner = u.id AND v.viewer = {:current_user_id}
+            WHERE u.id != {:current_user_id}
+              AND u.miniapp_name <> ''
+              AND u.user_photo <> ''
+              AND u.interests <> '[]'
+              AND NOT u.hide
+            GROUP BY u.id
+        ),
+        total_users AS (
+            SELECT COUNT(*) AS total FROM users WHERE id != {:current_user_id}
+        ),
+        limited_users AS (
+            SELECT * FROM user_view_dates 
+                     ORDER BY last_viewed
+                     ASC NULLS FIRST 
+                     LIMIT (
+                         SELECT MIN(100, MAX(3, CAST(total * 0.5 AS INT))) FROM total_users
+                     )
+        ) SELECT * FROM limited_users;
   `).bind({"current_user_id": e.auth.id}).all(views);
 
   console.log(JSON.stringify(views, null, 2));
 
   const response = require(`${__hooks}/worker.js`).request("POST", "feed/query", {
     id: e.auth.id,
-    from: views.map(view => view['pageOwner']),
+    from: views.map(view => view['user_id']),
   });
   if(response.statusCode !== 200) return e.json(response.statusCode, { response: response.json });
   const matches = response.json?.matches;
