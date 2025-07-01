5dcf86db5c186139bda681f9436ff75fe5f93e8b
Lowder <me@lowderplay.dev>
2025-07-02 02:19:06 +0500

    fix(TextWidget): remove uneven margin

--- src/lib/components/widgets/TextWidget.svelte
+++ src/lib/components/widgets/TextWidget.svelte
@@ -4,14 +4,8 @@
   let { data }: { data: Text } = $props();
 </script>
 
-<div class="TextBox text-container">
-  <p class="mx-[10px] break-words font-[Inter] text-[16px] italic font-medium">
+<div class="TextBox">
+  <p class="break-words font-[Inter] text-[16px] italic font-medium">
     {data.text}
   </p>
 </div>
-
-<style>
-  .text-container {
-    max-width: 300px;
-  }
-</style>
