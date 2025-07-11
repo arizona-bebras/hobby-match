46fdf90ea6b4d9d95f439c9a87e9c06d0b639162
Lowder <me@lowderplay.dev>
2025-07-11 18:37:46 +0500

    fix: sentry environment

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,6 +6,7 @@ import { PB_ADMIN_PASSWORD, PB_ADMIN_EMAIL } from '$env/static/private';
 Sentry.init({
   dsn: 'https://36e7202aadd5eb8af5b8392eb47b6bc2@o4509595179679744.ingest.de.sentry.io/4509595182956624',
   tracesSampleRate: 1,
+  environment: import.meta.env.PROD ? 'production' : 'development',
 });
 
 async function authAsAdmin() {
