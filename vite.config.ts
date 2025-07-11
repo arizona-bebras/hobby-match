d9fcb1b376a6229830b106e5f122d6857895ff4e
Lowder <me@lowderplay.dev>
2025-07-11 19:49:08 +0500

    ci: remove logging from build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
   plugins: [
     sentrySvelteKit({
       sourceMapsUploadOptions: {
