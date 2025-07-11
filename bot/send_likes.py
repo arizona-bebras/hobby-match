b1df295e9b562699b365059d87b48755562371a1
Lowder <me@lowderplay.dev>
2025-07-12 02:08:49 +0500

    feat(bot): send mutual likes to both parties

--- bot/send_likes.py
+++ bot/send_likes.py
@@ -15,6 +15,49 @@ DB_ADMIN_PASSWORD = os.getenv("DB_ADMIN_PASSWORD")
 bot = Bot(token=os.getenv('BOT_TOKEN'))
 app_url = os.getenv("APP_URL")
 
+
+def user_info(user):
+    gender = user.gender
+    name = (user.miniapp_name
+            .replace('_', '\\_')
+            .replace('*', '\\*')
+            .replace('[', '\\[')
+            .replace(']', '\\]')
+            .replace('(', '\\(')
+            .replace(')', '\\)')
+            .replace('~', '\\~')
+            .replace('`', '\\`')
+            .replace('>', '\\>')
+            .replace('#', '\\#')
+            .replace('+', '\\+')
+            .replace('-', '\\-')
+            .replace('=', '\\=')
+            .replace('|', '\\|')
+            .replace('{', '\\{')
+            .replace('}', '\\}')
+            .replace('.', '\\.')
+            .replace('!', '\\!')
+            .replace('\\', '\\\\'))
+    gender_postfix = 'а' if gender == 'female' else ''
+
+    profile_url = f"tg://user?id={user.telegram_id}"
+    name_link = f"_*[{name}]({profile_url})*_"
+    keyboard = InlineKeyboardMarkup(inline_keyboard = [
+        [
+            InlineKeyboardButton(
+                text=f"Написать",
+                url=profile_url)
+        ],
+        [
+            InlineKeyboardButton(
+                text="Посмотреть анкету",
+                web_app=WebAppInfo(url=f"{app_url}/Search?opened={user.id}"))
+        ]
+    ])
+
+    photo_url = pb.collection('users').get_file_url(user, user.user_photo, {"thumb": "350x350"})
+    return name, gender_postfix, name_link, keyboard, photo_url
+
 async def send_likes() -> None:
     pb.admins.auth_with_password(DB_ADMIN_LOGIN, DB_ADMIN_PASSWORD)
     likes = pb.collection('likes').get_full_list(query_params={
@@ -23,27 +66,6 @@ async def send_likes() -> None:
         "sort": "-created"
     })
     for like in likes:
-        gender = like.expand['user'].gender
-        name = (like.expand['user'].miniapp_name
-                .replace('_', '\\_')
-                .replace('*', '\\*')
-                .replace('[', '\\[')
-                .replace(']', '\\]')
-                .replace('(', '\\(')
-                .replace(')', '\\)')
-                .replace('~', '\\~')
-                .replace('`', '\\`')
-                .replace('>', '\\>')
-                .replace('#', '\\#')
-                .replace('+', '\\+')
-                .replace('-', '\\-')
-                .replace('=', '\\=')
-                .replace('|', '\\|')
-                .replace('{', '\\{')
-                .replace('}', '\\}')
-                .replace('.', '\\.')
-                .replace('!', '\\!')
-                .replace('\\', '\\\\'))
         is_mutual = False
         try:
             pb.collection('likes').get_first_list_item(f'user="{like.liked_user}" && liked_user="{like.user}"')
@@ -51,10 +73,7 @@ async def send_likes() -> None:
         except ClientResponseError:
             pass
 
-        gender_postfix = 'а' if gender == 'female' else ''
-
-        profile_url = f"tg://user?id={like.expand['user'].telegram_id}"
-        name_link = f"_*[{name}]({profile_url})*_"
+        name, gender_postfix, name_link, keyboard, photo_url = user_info(like.expand['user'])
         if is_mutual:
             text = ("*Взаимный лайк\\!* 💖\n\n" +
                     f"{name_link} поставил{gender_postfix} тебе лайк в ответ\\.\n" +
@@ -63,31 +82,24 @@ async def send_likes() -> None:
             text = ("*Твоя анкета кому\\-то понравилась\\!* ❤️\n\n" +
                     f"{name_link} поставил{gender_postfix} тебе лайк\\.")
 
-        keyboard = InlineKeyboardMarkup(inline_keyboard = [
-            [
-                InlineKeyboardButton(
-                    text=f"Написать",
-                    url=profile_url)
-            ],
-            [
-                InlineKeyboardButton(
-                    text="Посмотреть анкету",
-                    web_app=WebAppInfo(url=f"{app_url}/Search?opened={like.user}"))
-            ]
-        ])
+        pb.collection('likes').update(like.id, {
+            "sent": True,
+        })
         try:
-            photo_url = pb.collection('users').get_file_url(like.expand['user'],
-                                                            like.expand['user'].user_photo,
-                                                            {"thumb": "350x350"})
-
             await bot.send_photo(chat_id=like.expand['liked_user'].telegram_id,
                                  photo=photo_url,
                                  caption=text,
                                  reply_markup=keyboard,
                                  parse_mode=ParseMode.MARKDOWN_V2)
-            pb.collection('likes').update(like.id, {
-                "sent": True,
-            })
+            if is_mutual:
+                name, gender_postfix, name_link, keyboard, photo_url = user_info(like.expand['user'])
+                await bot.send_photo(chat_id=like.expand['user'].telegram_id,
+                                     photo=photo_url,
+                                     caption="*Взаимный лайк\\!* 💖\n\n" +
+                                             f"{name_link} тоже поставил{gender_postfix} тебе лайк\\.\n" +
+                                             "Приятного общения\\!",
+                                     reply_markup=keyboard,
+                                     parse_mode=ParseMode.MARKDOWN_V2)
         except TelegramError as e:
             print(e)
     
