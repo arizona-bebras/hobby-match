c41d2bc63a29ac13168759fe37918c3807704df6
XrystikLK <23spiisiinii@gmail.com>
2025-12-17 19:12:00 +0500

    backend change

--- src/routes/(MainApp)/profile/+page.ts
+++ src/routes/(MainApp)/profile/+page.ts
@@ -5,25 +5,28 @@ import type { PageData } from '$lib/questionnaireTypes/questionnaireTypes';
 
 export const load: PageLoad = async ({ fetch, data, depends }) => {
   depends('user:widgets');
-  const authHeader: HeadersInit = new Headers()
-  authHeader.set('Authorization', `Bearer ${window.localStorage.getItem("access_token")}`)
+  const authHeader: HeadersInit = new Headers();
+  authHeader.set(
+    'Authorization',
+    `Bearer ${window.localStorage.getItem('access_token')}`,
+  );
   const res = await fetch(`${db}/api/me`, {
-    method: "GET",
-    headers: authHeader
-  }).then(res => res.json())
+    method: 'GET',
+    headers: authHeader,
+  }).then((res) => res.json());
   const currentDate = new Date().getTime() / 1000;
-  const birthDate = new Date(res.birth_date).getTime()/1000;
-  const age = currentDate - birthDate
-  var pageData: PageData = res as PageData
-  pageData.age = Math.floor(age / (60 * 60 * 24 * 365.25))
-  for (var widget of pageData.widgets) {
-    widget.data = JSON.parse(widget.data)
-    if (widget.additionalData != "") {
-      widget.additionalData = JSON.parse(widget.additionalData)
+  const birthDate = new Date(res.birth_date).getTime() / 1000;
+  const age = currentDate - birthDate;
+  const pageData: PageData = res as PageData;
+  pageData.age = Math.floor(age / (60 * 60 * 24 * 365.25));
+  for (const widget of pageData.widgets) {
+    widget.data = JSON.parse(widget.data);
+    if (widget.additionalData != '') {
+      widget.additionalData = JSON.parse(widget.additionalData);
     }
   }
   pageData.widgets = pageData.widgets.sort((a, b) => {
-    return a.order - b.order
-  })
-  return pageData
-}
+    return a.order - b.order;
+  });
+  return pageData;
+};
