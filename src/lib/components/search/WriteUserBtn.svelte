568ab8578598e98314695a80aa6082679d984bd1
XrystikLK <23spiisiinii@gmail.com>
2026-01-12 23:22:56 +0500

    feat(view): change "send user image message" button icon

--- src/lib/components/search/WriteUserBtn.svelte
+++ src/lib/components/search/WriteUserBtn.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { UserPen } from '@lucide/svelte';
+  import { UserPen, Mail } from '@lucide/svelte';
   let { username }: { username: string } = $props();
   // window.Telegram.WebApp.openTelegramLink('tg://user?id=5000721686');
   let href = $derived(`https://t.me/${username}`);
@@ -9,5 +9,5 @@
   {href}
   class="fixed bottom-20 right-5.5 bg-accent size-12.5 flex items-center justify-center rounded-xl"
 >
-  <UserPen class="size-6 text-text-color" />
+  <Mail class="size-6 text-text-color" />
 </a>
